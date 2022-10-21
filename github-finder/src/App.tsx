e35504962f200f471d289fa8a5688550c2cbc1d4
Lilo <liloelia@gmail.com>
2022-10-21 20:41:10 +0000

    - added layout - added solution for HMR (see .env) - added gitpod config to auto run server and preview

--- github-finder/src/App.tsx
+++ github-finder/src/App.tsx
@@ -1,9 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Footer } from './components/layout/Footer';
+import { Navbar } from './components/layout/Navbar';
+
 function App() {
   return (
-    <div className="bg-purple-500">
-      <h1 className="text-xl">Hello Lilo</h1>
-      <button className="btn">Click me</button>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route
+              path="/"
+              element={<div className="text-xl mx-auto block w-40 border-dashed border-2 border-blue-300 text-center">Main content goes here</div>}
+            />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
